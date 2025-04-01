056ceb231a905b8f0fcd29b34cde9bbcf2c0beab
rm-rf-SeanSalvo <salvo.284024@globalcity.sti.edu.ph>
2025-04-01 16:12:54 +0800

    Add project files.

---
+++ InventoryManagementSystem2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventoryManagementSystem2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
