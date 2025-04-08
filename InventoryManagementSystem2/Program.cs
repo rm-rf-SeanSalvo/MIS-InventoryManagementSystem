e482174776f15476f9435338bc5245913062ea19
rm-rf-SeanSalvo <salvo.284024@globalcity.sti.edu.ph>
2025-04-08 14:10:33 +0800

    ✅ Authentication Page ready ✅ Layouts fixed ✅ Authentication Backend Logic being fixed still.
    - Salvo

--- InventoryManagementSystem2/Program.cs
+++ InventoryManagementSystem2/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 // Define routing for the controllers
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=LoginPage}/{id?}");
 
 app.Run();
