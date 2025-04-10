17ca74921a721785934f879336ccb25789eb0128
rm-rf-SeanSalvo <salvo.284024@globalcity.sti.edu.ph>
2025-04-10 16:42:05 +0800

    ✅ Login Page fully functional ✅ Admin Dashboard CRUD functional.
    - Salvo

---
+++ InventoryManagementSystem2/Controllers/AdminPageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementSystem2.Controllers
+{
+    // AdminController.cs
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Logic for Dashboard page
+            return View();
+        }
+
+        public IActionResult Employees()
+        {
+            // Logic for Employees page
+            return View();
+        }
+
+        public IActionResult Purchase()
+        {
+            // Logic for Purchase page
+            return View();
+        }
+
+        public IActionResult Stock()
+        {
+            // Logic for Stock page
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            // Logic for Menu page
+            return View();
+        }
+
+        public IActionResult OrderList()
+        {
+            // Logic for Order List page
+            return View();
+        }
+    }
+
+}
