95e3ea41a0c9f05fa4819da89d7a487967a9debc
rm-rf-SeanSalvo <salvo.284024@globalcity.sti.edu.ph>
2025-04-12 11:43:10 +0800

    ✅ Employee GUI done. ✅ Dashboard GUI done. ✅ Menu GUI done. ✅ OrderList for Customer Information GUI done. ✅ Purchase GUI done. ✅ Stock GUI done.
    - Salvo

---
+++ InventoryManagementSystem2/Controllers/OrderListController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class OrderListController : Controller
+{
+    public IActionResult Index()
+    {
+        // You could pass your order list model here
+        return View();
+    }
+
+    public IActionResult OrderDetails(int id)
+    {
+        // Normally you'd retrieve order details using the ID
+        // For now, we'll just pass the id to the view as a placeholder
+        ViewBag.OrderId = id;
+        return View();
+    }
+}
